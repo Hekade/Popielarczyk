d48e7d22813c967d81b92622867041862e6d4cea
Hekade <popielarczykbartek@gmail.com>
2019-06-27 07:19:54 +0200

    Add files via upload

---
+++ Parameters.php
@@ -0,0 +1,6 @@
+<?php
+
+const SERVERNAME = 'db4free.net'; // db4free.net
+const USERNAME = 'popielarczykb'; // popielarczykb
+const PASSWORD = 'ZALICZENIE1'; // ZALICZENIE1
+const DATABASE = 'bpopiel2019'; // BazaDanychPAI219 BPopiel201901
